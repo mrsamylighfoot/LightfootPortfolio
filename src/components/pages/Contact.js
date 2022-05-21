90d4fbe2f5641b14cc4005a17f2469bc9a370d97
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-21 14:48:10 -0400

    skeleton placeholders for portfolio page setup

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        LinkedIn:
+        GitHub repositories:
+        Email:
+      </p>
+    </div>
+  );
+}
