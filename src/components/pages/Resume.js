a52208f7134cb91e86b11bb7318750a69eccd559
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-22 13:39:30 -0400

    updating component page titles and updating references

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Amy Lightfoot Resume</h1>
+      <p>
+        Amy Lightfoot is a nurse turned coder.  
+      </p>
+    </div>
+  );
+}
